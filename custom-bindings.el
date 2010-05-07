2d254e286d8ae19f19251784bf4b773ff42c4017
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2010-05-07 11:08:52 +0200

    don't rebind reverse search

--- custom-bindings.el
+++ custom-bindings.el
@@ -1,7 +1,7 @@
 ;;; custom-bindings.el --- my custom key bindings
 
 (global-set-key  [C-tab] 'other-window)
-(global-set-key (kbd "C-r") 'replace-string)
+;(global-set-key (kbd "C-r") 'replace-string)
 (global-set-key (kbd "C-c C-g") 'gist-buffer-confirm)
 (global-set-key (kbd "C-x a a") 'ack)
 
