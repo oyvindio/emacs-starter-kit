6a17a98a0c0d17fcc011fc8f518b4343144f26f2
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2013-03-03 21:33:37 +0100

    new Python mode conf
    * use jedi for completion
    * remove ropemacs and pymacs :feelsgood:
    * simpler flymake wrapper
    * use regular python repl, not ipython

---
+++ bin/python-flymake.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ -z $(which -s pylint) ]]
+then
+    pylint "$1" 2>/dev/null
+fi
+if [[ -z $(which -s pyflakes) ]]
+then
+    pyflakes "$1"
+fi
+if [[ -z $(which -s pep8) ]]
+then
+    # http://pep8.readthedocs.org/en/latest/intro.html#error-codes
+    pep8 --ignore=E701,E501 --repeat "$1"
+fi
+true
--- starter-kit-python.el
+++ starter-kit-python.el
@@ -1,14 +1,5 @@
 ;;; starter-kit-python.el
 
-;; For this to work properly, you need:
-;; * the ipython executable on your $PATH (for ipython buffer)
-;; * pep8, pylint and pyflakes on your $PATH (for
-;; pylint_etc_wrapper.py). these can be installed with pip.
-;;
-;; Note: To explicitly tell pymacs which python it should use,
-;; set the PYMACS_PYTHON environment variable to the full path to the
-;; python executable.
-
 ;; Load python-mode for .py and .wsgi files
 (autoload 'python-mode "python-mode" "Python editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
@@ -24,55 +15,19 @@
     (add-to-list 'ffip-patterns "*.py")
     (add-to-list 'ffip-patterns "*.wsgi")))
 
-
-;; Fix broken tab completion in ipython buffer
-(setq ipython-completion-command-string
-      "print(';'.join(__IP.Completer.all_completions('%s')))\n")
-
 ;;; Flymake
-;; Run `pyflymake` (pylint, pyflakes and pep8) on python files with flymake
-(setq flymake-executable (concat dotfiles-dir "/bin/" "pylint_etc_wrapper.py"))
+(setq flymake-executable (concat dotfiles-dir "bin/python-flymake.sh"))
 (when (load "flymake" t)
   (defun flymake-python-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
                      'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
+           (local-file (file-relative-name temp-file
                       (file-name-directory buffer-file-name))))
     (list flymake-executable (list local-file))))
   (push '(".+\\.py$" flymake-python-init) flymake-allowed-file-name-masks))
 
-;;; Pymacs
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-
-(when (eq system-type 'darwin)
-  (eval-after-load "pymacs"
-    (setq pymacs-python-command "/usr/local/bin/python")))
-
-
-
-(defun lazy-load-ropemacs-mode ()
-  "Activates ropemacs-mode. Loads ropemacs through pymacs if not
-loaded yet."
-  (when (not (fboundp 'ropemacs-mode))
-    (pymacs-load "ropemacs" "rope-"))
-  (ropemacs-mode t))
-
-(eval-after-load "ropemacs"
-  (lambda ()
-    ;; don't ask permission to save buffer before running refactorings
-    (setq ropemacs-confirm-saving nil)
-    ;; try to open ropemacs project files automagically
-    (setq ropemacs-guess-project t)))
-
 (add-hook 'python-mode-hook
           (lambda ()
-            ; load ropemacs, but only once!
-            (lazy-load-ropemacs-mode)
             ; Activate flymake unless buffer is a tmp buffer for the interpreter
             (unless (eq buffer-file-name nil) (flymake-mode t))
             ;; Bind a few keys for navigating errors
@@ -81,13 +36,23 @@ loaded yet."
             (local-set-key (kbd "M-p") 'flymake-goto-prev-error)
             (load-library "flymake-no-cursor")))
 
-;;; Use ipython
-(when (executable-find "ipython")
-    (require 'ipython)
-    (setq py-python-command-args '("")))
+;; jedi
+(add-to-list 'load-path (concat dotfiles-dir "opt/emacs-jedi-03d26925d0/"))
+(setq jedi:setup-keys t)
+(setq jedi:key-complete (kbd "M-/"))
+(setq jedi:key-goto-definition (kbd "M-g d"))
 
+(autoload 'jedi:setup "jedi" nil t)
+(add-hook 'auto-complete-mode-hook (lambda ()
+                                     (define-key ac-completing-map (kbd "C-n") 'ac-next)
+                                     (define-key ac-completing-map (kbd "C-p") 'ac-previous)))
+(add-hook 'python-mode-hook 'auto-complete-mode)
+(add-hook 'python-mode-hook 'jedi:setup)
 
-;; TODO: bindings for pydoc, a-la ri in starter-kit-ruby.el
-;; TODO: self-contained pymacs+ropemacs?
+(require 'nose)
+(eval-after-load 'python-mode
+  '(progn
+     (define-key py-mode-map (kbd "M-?") 'hippie-expand)
+     (define-key py-mode-map (kbd "C-c t") 'nosetests-all)))
 
 (provide 'starter-kit-python)
