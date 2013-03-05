9173b6a6865e56d96c3e1555a2b02c67ad39aa0f
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2013-03-05 18:30:42 +0100

    xml mode indent

--- starter-kit-xml.el
+++ starter-kit-xml.el
@@ -1,6 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 
-
+(setq sgml-basic-offset 4)
 (add-hook 'nxml-mode-hook
           (lambda ()
             (local-set-key (kbd "C-c C-n") 'pretty-print-xml-region)))
