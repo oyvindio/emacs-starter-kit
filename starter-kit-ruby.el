48eed2142bf9aaefb4d4f547c4e7b563e91f3642
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2014-01-24 16:04:57 +0100

    ruby-mode for .spec files

--- starter-kit-ruby.el
+++ starter-kit-ruby.el
@@ -25,6 +25,7 @@
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Buildfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.spec$" . ruby-mode))
 
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
