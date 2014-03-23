ad155ac80bf7f2c0fe3a5fc38991240ea38927aa
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2014-03-23 20:17:43 +0100

    Give useful names to buffers with common filenames

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -168,6 +168,25 @@
 (add-hook 'change-major-mode-hook 'underscore-as-word-separator)
 
 
+(setq rename-file-buffers-alist
+      '(("pom.xml" . "pom")
+	("setup.py" . "setup")
+	("Makefile" . "make")
+	("__init__.py" . "pkg")))
+
+(defun detect-and-rename-file-buffers ()
+  (mapcar (lambda (acons)
+	    (detect-and-rename-file-buffer (car acons) (cdr acons)))
+	  rename-file-buffers-alist))
+
+(defun detect-and-rename-file-buffer (filename buffer-suffix)
+  (when (string= (buffer-name) filename)
+    (let* ((folders (split-string (buffer-file-name) "/"))
+	   (base-dir (car (last folders 2))))
+      (rename-buffer (format "%s-%s" base-dir buffer-suffix)))))
+
+(add-hook 'find-file-hook 'detect-and-rename-file-buffers)
+
 (defun goto-line-with-feedback ()
   "Show line numbers temporarily, while prompting for the line number input"
   (interactive)
